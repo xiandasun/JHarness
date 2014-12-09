b6c1d3ec19a31ff74a0f0b4a7132f262ab558b59
Xianda <xianda@Thomas-Los-MacBook-Pro-15.local>
2014-12-09 15:11:47 -0500

    add dictionaries

---
+++ src/main/java/dictionaries/RwLockDictionary.java
@@ -0,0 +1,47 @@
+package dictionaries;
+
+import java.util.HashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import data.Node;
+
+public class RwLockDictionary extends IDictionary {
+	
+	HashMap<Integer, Node> internalMap;
+	ReadWriteLock lock;
+
+	public RwLockDictionary() {
+		internalMap = new HashMap<Integer, Node>();
+		lock = new ReentrantReadWriteLock();
+	}
+	
+	@Override
+	public void put(int k, Node v) {
+		lock.writeLock().lock();
+		internalMap.put(k, v);
+		lock.writeLock().unlock();
+	}
+
+	@Override
+	public Node get(int k) {
+		Node ret = null;
+		lock.readLock().lock();
+		ret = internalMap.get(k);
+		lock.readLock().unlock();
+		return ret;
+	}
+
+	@Override
+	public void remove(int k) {
+		lock.writeLock().lock();
+		internalMap.remove(k);
+		lock.writeLock().unlock();
+	}
+
+	@Override
+	public int size() {
+		return internalMap.size();
+	}
+
+}
